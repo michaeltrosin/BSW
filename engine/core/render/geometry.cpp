5389c480e994837fc86141d83dbf67a6025791c9
Ra6nar0k21 | Michael <38357618+michaeltrosin@users.noreply.github.com>
2021-03-01 21:25:02 +0100

    Removed buggy geometry rendering

--- engine/core/render/geometry.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Michael on 23.02.2021.
-//
-
-#include "geometry.h"
-uint32_t Geometry::index_count() const { return m_vertices.size(); }
-
-void Geometry::set_data(const std::vector<glm::vec4> &vertices) { m_vertices = vertices; }
