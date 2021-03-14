c84e5332853739fafd844450784dfe7251cb8134
Ra6nar0k21 | Michael <38357618+michaeltrosin@users.noreply.github.com>
2021-03-14 02:38:50 +0100

    Added a new Macro, which makes searching if a key exists in an unsigned map more easily

--- engine/utils/macros.h
+++ engine/utils/macros.h
@@ -5,10 +5,12 @@
 
 #define BIT(x) (1 << (x))
 
-#define ARRAY_SIZE(arr) ((int) (sizeof(arr) / sizeof(*(arr))))
+#define ARRAY_SIZE(arr) ((int) (sizeof(arr) / sizeof(*(arr)))
 
 #define BITFIELD(enum)                                                                                                                               \
     inline enum operator|(enum a, enum b) { return static_cast<enum>(static_cast<uint32_t>(a) | static_cast<uint32_t>(b)); }                         \
     inline bool operator&(enum a, enum b) { return static_cast<uint32_t>(a) & static_cast<uint32_t>(b); }                                            \
     inline bool operator==(enum a, enum b) { return a & b; }                                                                                         \
     inline bool operator==(enum a, uint32_t b) { return (uint32_t) a & b; }
+
+#define UMAP_CONTAINS(map, search) ((map).find(search) != (map).end())
